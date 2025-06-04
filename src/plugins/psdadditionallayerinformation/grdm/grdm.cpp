68e2783f57864dc62e245791607ce81b78993bb0
HIDAKA Takahiro <takahr@gmail.com>
2025-06-05 05:20:48 +0900

    add grdm plugin for Gradient Map

---
+++ src/plugins/psdadditionallayerinformation/grdm/grdm.cpp
@@ -0,0 +1,96 @@
+// Copyright (C) 2025 Signal Slot Inc.
+// SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include <QtPsdCore/qpsdadditionallayerinformationplugin.h>
+
+QT_BEGIN_NAMESPACE
+
+class QPsdAdditionalLayerInformationGrdmPlugin : public QPsdAdditionalLayerInformationPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QPsdAdditionalLayerInformationFactoryInterface" FILE "grdm.json")
+public:
+    // Gradient Map
+    QVariant parse(QIODevice *source , quint32 length) const override {
+        auto cleanup = qScopeGuard([&] {
+            Q_ASSERT(length <= 3);
+        });
+
+        const auto version = readU16(source, &length);
+        Q_ASSERT(version == 1);
+        const auto reversed = readU8(source, &length);
+        Q_UNUSED(reversed);
+        const auto dithered = readU8(source, &length);
+        Q_UNUSED(dithered);
+        const auto name = readString(source, &length);
+        Q_UNUSED(name);
+
+        const auto countColorStops = readU16(source, &length);
+        for (int i = 0; i < countColorStops; i++) {
+            const auto location = readU32(source, &length);
+            Q_UNUSED(location);
+            const auto midpoint = readU32(source, &length);
+            Q_UNUSED(midpoint);
+            const auto color = readColor(source, &length);
+            Q_UNUSED(color);
+
+            skip(source, 2, &length);
+        }
+
+        const auto countTransparencyStops = readU16(source, &length);
+        for (int i = 0; i < countTransparencyStops; i++) {
+            const auto location = readU32(source, &length);
+            Q_UNUSED(location);
+            const auto midpoint = readU32(source, &length);
+            Q_UNUSED(midpoint);
+            const auto tramsparency = readU16(source, &length);
+            Q_UNUSED(tramsparency);
+        }
+
+        const auto expansionCount = readU16(source, &length);
+        Q_ASSERT(expansionCount == 2);
+
+        const auto interpolation = readU16(source, &length);
+        Q_UNUSED(interpolation);
+        const auto len = readU16(source, &length);
+        Q_ASSERT(len == 32);
+        const auto gradientMode = readU16(source, &length);
+        Q_UNUSED(gradientMode);
+        const auto randomSeed = readU32(source, &length);
+        Q_UNUSED(randomSeed);
+        const auto showTransparency = readU16(source, &length);
+        Q_UNUSED(showTransparency);
+        const auto useVectorColor = readU16(source, &length);
+        Q_UNUSED(useVectorColor);
+        const auto roughness = readU32(source, &length);
+        Q_UNUSED(roughness);
+        const auto colorModel = readU16(source, &length);
+        Q_UNUSED(colorModel);
+
+        const auto minColor1 = readU16(source, &length);
+        Q_UNUSED(minColor1);
+        const auto minColor2= readU16(source, &length);
+        Q_UNUSED(minColor2);
+        const auto minColor3 = readU16(source, &length);
+        Q_UNUSED(minColor3);
+        const auto minColor4 = readU16(source, &length);
+        Q_UNUSED(minColor4);
+
+        const auto maxColor1 = readU16(source, &length);
+        Q_UNUSED(maxColor1);
+        const auto maxColor2 = readU16(source, &length);
+        Q_UNUSED(maxColor2);
+        const auto maxColor3 = readU16(source, &length);
+        Q_UNUSED(maxColor3);
+        const auto maxColor4 = readU16(source, &length);
+        Q_UNUSED(maxColor4);
+
+        skip(source, 2, &length);
+
+        return {};
+    }
+};
+
+QT_END_NAMESPACE
+
+#include "grdm.moc"
