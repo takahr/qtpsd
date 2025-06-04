f1f7e950f32450c66913d0d225c90ce0640614eb
HIDAKA Takahiro <takahr@gmail.com>
2025-06-04 12:24:30 +0900

    add u16 plugin for Posterize and Threshold

---
+++ src/plugins/psdadditionallayerinformation/u16/u16.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2024 Signal Slot Inc.
+// SPDX-License-Identifier: LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include <QtPsdCore/qpsdadditionallayerinformationplugin.h>
+
+QT_BEGIN_NAMESPACE
+
+class QPsdAdditionalLayerInformationU16Plugin : public QPsdAdditionalLayerInformationPlugin
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QPsdAdditionalLayerInformationFactoryInterface" FILE "u16.json")
+public:
+    // Layer ID
+    QVariant parse(QIODevice *source , quint32 length) const override {
+        auto cleanup = qScopeGuard([&] {
+            Q_ASSERT(length == 0);
+        });
+        const auto res = readU16(source, &length);
+        // Padding
+        skip(source, 2, &length);
+        return res;
+    }
+};
+
+QT_END_NAMESPACE
+
+#include "u16.moc"
